6569efaab526056b1e92891303c6b940df96eb11
Yuraul <yuraulio@gmail.com>
2020-12-30 00:31:29 +0200

    Months change handler.

--- js/superform.js
+++ js/superform.js
@@ -32,16 +32,27 @@
           }
           return values;
         }
+        // Якщо немає порожніх місяців цього кварталу,
+        // 1) обраховуємо квартал за формулою;
+        // 2) порівнюємо це значення із введеним і
+        // якщо воно не порожнє і виходить за межі
+        // показуємо повідомлення і виправляємо значення;
+        // 3) запускаємо подію зміни підсумку року.
         if ((index % 4) === 0) {
           values = getValues(el.parent().parent());
-          console.log(values);
           if (values) {
             let tmpQuarter = calcTotal(values);
             if (Math.abs(tmpQuarter - elValue) > 0.05) {
               alert('Deviation is too big. Value will be set to computed.');
+              // Додати перевірку на порожнє значення.
               el.val(tmpQuarter.toFixed(2));
             }
           }
+          // Якщо немає порожніх квартальних значень,
+          // 1) обраховуємо рік за формулою;
+          // 2) порівнюємо це значення із введеним і
+          // якщо воно не порожнє і виходить за межі,
+          // показуємо повідомлення і виправляємо значення;
         } else if (index === 17) {
           values = getValues();
           console.log(values);
@@ -49,11 +60,22 @@
             let tmpYear = calcTotal(values);
             if (Math.abs(tmpYear - elValue) > 0.05) {
               alert('Deviation is more then 0.05. Value will be set to computed.');
+              // Додати перевірку на порожнє значення.
               el.val(tmpYear.toFixed(2));
             }
           }
+          // Якщо немає порожніх місяців відповідного кварталу,
+          // 1) запускаємо подію зміни кварталу.
+        } else {
+          let quarter = $("#super_form tr td").eq(((index / 4 >> 0) + 1) * 4);
+          values = getValues(quarter);
+          if (values) {
+            let quarterTotal = calcTotal(values);
+            console.log(quarter);
+            $($(quarter.children()[0]).children()[0]).val(quarterTotal.toFixed(2));
+            $($(quarter.children()[0]).children()[0]).triggerHandler('change');
+          }
         }
-
       });
     }
   };
